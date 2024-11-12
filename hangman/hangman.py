3cbabe9aeb207ee8d105a8f3d3816cfa6e58b45b
ВашÐEducc-cmd <n.s.pashchenko@student.itc.khai.edu>
2024-11-13 00:06:09 +0200

    Hangman 5-st stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -6,18 +6,39 @@ d_s()
 
 import random
 
+
 def play():
     print("HANGMAN")
     words = ['python', 'java', 'javascript', 'php']
     secret_word = random.choice(words)
-    hint = secret_word[:3] + "-" * (len(secret_word) - 3)
+    guessed_word = ["-"] * len(secret_word)
+    attempts = 8
+    guessed_letters = set()
 
-    print(f"Guess the word {hint}: ", end="")
-    guess = input("> ")
+    while attempts > 0:
+        print("\n" + "".join(guessed_word))
+        guess = input("Введіть букву: > ")
 
-    if guess.lower() == secret_word:
-        print("Ти вгадав!")
-    else:
-        print("Спробуй ще раз!")
+        if guess in guessed_letters:
+            print("Ви вже вгадали букву")
+        else:
+            guessed_letters.add(guess)
+
+            if guess in secret_word:
+                for i, letter in enumerate(secret_word):
+                    if letter == guess:
+                        guessed_word[i] = guess
+            else:
+                attempts -= 1
+                print("Такої літери немає в данному слові")
+
+        if "".join(guessed_word) == secret_word:
+            print("\n" + secret_word)
+            print("Ти вгадав!")
+            return
+
+    print("Ти програв!")
+    print("Дякую за гру!")
+    print("Наступне завдання буде важче :).")
 
 play()
