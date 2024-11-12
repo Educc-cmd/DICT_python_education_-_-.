13cbc31b956b1695b0b85e35ca5a50462bf9895e
ВашÐEducc-cmd <n.s.pashchenko@student.itc.khai.edu>
2024-11-12 23:21:19 +0200

    Hangman 2-st stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -3,3 +3,16 @@ def d_s():
     print("The game will be available soon.")
 
 d_s()
+
+def play():
+    print("HANGMAN")
+    secret_word = "python"
+    guess = input("Спробуй вгадати слово: > ")
+
+    if guess.lower() == secret_word:
+        print("Ти вгадав!")
+    else:
+        print("Спробуй ще раз!")
+
+d_s()
+play()
